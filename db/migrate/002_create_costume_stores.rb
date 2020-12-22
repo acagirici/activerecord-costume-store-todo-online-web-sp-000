ae9fe65876fbcb002ea6c5d0b9c166ce2949faae
Arif Cagirici <acagirici23@gmail.com>
2020-12-22 05:42:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.string :image_url
+      t.text :image_url
       t.timestamps
     end
   end
