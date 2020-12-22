3a3272e9e8032cfef7c9c53ea32edba01ad028d2
Arif Cagirici <acagirici23@gmail.com>
2020-12-22 05:06:57 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,8 +6,7 @@ class Costume < ActiveRecord::Migration[5.2]
       t.string :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
