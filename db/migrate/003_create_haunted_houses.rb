b8422bf9099b70c86ccd7656189c3d8b11b40b2e
Arif Cagirici <acagirici23@gmail.com>
2020-12-22 05:52:51 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,14 @@
-class HauntedHouse < ActiveRecord::Migration[5.2]
+class CreateHauntedHouse < ActiveRecord::Migration[5.2]
   
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
 end
